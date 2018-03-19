b3f6ca42188cc33330063028fc84b55d186aa14e
Tyler Lindell <tlindell@tesla.com>
2018-03-19 08:37:05 -0700

    updating package-lock and removing red cube from scene

--- src/vrScene.js
+++ src/vrScene.js
@@ -21,7 +21,6 @@ class VRScene extends React.Component {
           <a-asset-item id="forklift-obj" src={forkliftobj}></a-asset-item>
           <a-asset-item id="forklift-mtl" src={forkliftmtl}></a-asset-item>
         </a-assets>
-        <a-box color="red" rotation="0 45 45" scale="2 2 2" position="0 0 -10"></a-box>
         <a-light type= "point" position="0 5 5"/>
         {/* <a-plane position="0 0 -4" rotation="-90 0 0" width="400" height="400" color="#7BC8A4"></a-plane> */}
         <a-sky color="#ECECEC"></a-sky>
